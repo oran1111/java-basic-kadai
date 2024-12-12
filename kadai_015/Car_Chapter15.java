ce97e6f92b3126506cbfadd9585c79e8e6847be6
oran1111 <akkooiikkee@gmail.com>
2024-12-12 16:40:43 +0900

    Car_Chapter15.javaを修正

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,10 +1,11 @@
 package kadai_015;
 
 public class Car_Chapter15 {
-	private int gear = 0;
+	private int gear = 1;
 	private int speed = 0;
 	
 	public void gearChange(int afterGear) {
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
 		this.gear = afterGear;
 		switch (this.gear) {
 			case 1 -> this.speed = 10;
@@ -17,6 +18,6 @@ public class Car_Chapter15 {
 	}
 	
 	public void run() {
-		System.out.println("速度：時速" + this.speed + "km");
+		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 }
